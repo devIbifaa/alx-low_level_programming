f7408d6e11e1a16a4e920015b68ed64dd798c3d6
devIbifaa <ib2link@yahoo.com>
2023-08-11 07:07:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+
 /**
- *Main: a C program that prints a line with the function put
- *Return: should return the value 0
-*/
+ * main - Entry point of the program
+ *
+ * Description: This is the main function that serves as the entry point of
+ * the program. It prints a message to the console.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle,\n");
