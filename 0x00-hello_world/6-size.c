9c06ecd53ac3b0c6144c54c41575cea47e3f9aef
devIbifaa <ib2link@yahoo.com>
2023-08-11 07:35:35 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function is the entry point of the program. It prints
+ * a message to the console.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
