1ae6e268c3adf9c404f64d0c1a37862cc7204148
devIbifaa <ib2link@yahoo.com>
2023-08-12 23:55:15 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Description: Program loop through the alphabet & print a character
+* Return: 0 (Success)
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+putchar('\n');
+}
+return (0);
+}
