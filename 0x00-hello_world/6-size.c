ac9fe9256d34d360816981ba82e8df17e06e225e
devIbifaa <ib2link@yahoo.com>
2023-08-20 06:28:19 -0700

    1-last_digit.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,15 @@
 
 /**
  * main - Entry point of the program
- *
  * Description: This function is the entry point of the program. It prints
- * a message to the console.
- *
  * Return: 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("Size of a char: %1u byte(s)\n" sizeof(char));
+printf("Size of an int: %1u byte(s)\n", sizeof(int));
+printf("Size of a long int: %1u byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %1u byte(s)\n", sizeof(long long int));
+printf("Size of a float: %1u byte(s)\n", sizeof(float));
 return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ n = rand() - RAND_MAX / 2;
 ld = n % 10;
 if (ld > 5)
 {
-printf("Last of %d  is %d and is greater than 5 and not 0", n, ld);
+printf("Last of %d is %d and is greater than 5 and not 0", n, ld);
 }
 else if (ld == 0)
 {
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and is 0", n, ld);
 }
 else
 {
-printf("Last of %d  is %d and is less than 5 and not 0", n, ld);
+printf("Last of %d is %d and is less than 5 and not 0", n, ld);
 }
 return (0);
 }
