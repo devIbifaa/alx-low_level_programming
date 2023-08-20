4b8980e419387a474ae15c41ec9b7730e68ee68e
devIbifaa <ib2link@yahoo.com>
2023-08-20 08:26:07 -0700

    correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ n = rand() - RAND_MAX / 2;
 ld = n % 10;
 if (ld > 5)
 {
-printf("Last digit of %d is %d and is greater than 5 and not 0\n", n, ld);
+printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 }
 else if (ld == 0)
 {
