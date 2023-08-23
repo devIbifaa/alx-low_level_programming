7b862b4b64c47b68d0bc3e635f9da89982b46cc5
devIbifaa <ib2link@yahoo.com>
2023-08-22 21:59:48 -0700

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,32 +9,33 @@
  */
 int _atoi(char *s)
 {
-int result = 0;
-int sign = 1;
+int c = 0
+unsigned int ni = 0;
+int min = 1;
+int isi = 0;
 
-while (*s != '\0') {
-if (*s == '-') {
-sign = -1;
-}
-else if (*s >= '0' && *s <= '9')
+while(s[c])
 {
-int digit = *s - '0';
-
-if (result > (INT_MAX - digit) / 10)
+if (s[c] == 45)
 {
-return (sign == 1) ? INT_MAX : INT_MIN;
+min *= -1;
 }
-result = result * 10 + digit;
-}
-else if (*s != '+' && *s != ' ' && *s != '\t')
+while (s[c] >=  48 && s[c] <= 57)
 {
-
+isi = 1;
+ni = (ni * 10) + (s[c] - '0');
+c++;
+}
+if (isi == 1)
+{
 break;
 }
-s++;
+c++;
 }
+ni *= min;
+return (ni);
 
- return (result *sign);
 }
+     
 
 
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,19 +9,12 @@
 void print_array(int *a, int n)
 {
 int i;
-if (n <= 0 || a == NULL)
-{
-return;
-}
-
-
-for (i = 0; i < n; i++)
+for(i = 0; i < n; i++)
 {
 printf("%d", a[i]);
-
-if (i < n - 1)
+if (i != (n - 1))
 {
-printf(", ");
+printf(",");
 }
 }
 printf("\n");
