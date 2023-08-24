bd773c87c9db6d8c1832977e04fc5794321d6c08
devIbifaa <ib2link@yahoo.com>
2023-08-24 05:28:29 -0700

    update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,7 +3,8 @@
 /**
  * rot13 - Encodes a string using ROT13 cipher.
  * @str: The input string to be encoded.
- *
+ * @data1: sorted data
+ * @scataData: unsorted data
  * Return: A pointer to the modified string.
  */
 
@@ -18,7 +19,7 @@ for (i = 0; str[i] != '\0'; i++)
 {
 for (j = 0; j < 52; j++)
 {
-if(str[i] == data1[j])
+if (str[i] == data1[j])
 {
 str[i] = scataData[j];
 break;
